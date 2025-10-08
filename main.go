4922e198320661520da64adde56d1b1487d50993
NoelSaludo <23-08992@g.batstate-u.edu.ph>
2025-10-08 15:21:41 +0800

    gin framework added

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		// Return JSON response
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run()
 }
